4bb14843383fc599bd187b16829c1dc3ad8a6d76
Aymane El yamani <aymaneelyamani3@gmail.com>
2024-04-03 02:56:43 +0100

    basic cachine

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,10 +7,12 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """ class documenation """
     def put(self, key, item):
+        """ put documentation"""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """get documentation """
         if key:
             return self.cache_data.get(key)
         else:
